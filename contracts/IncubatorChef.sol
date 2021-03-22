9d047e82743432cfe7df23f1b92c443c2649b325
Kswap Farm <77991984+okexfarm@users.noreply.github.com>
2021-03-22 16:51:23 +0530

    Update IncubatorChef.sol

--- contracts/IncubatorChef.sol
+++ contracts/IncubatorChef.sol
@@ -20,10 +20,10 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         uint256 amount;         // How many LP tokens the user has provided.
         uint256 rewardDebt;     // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of GOOSEs
+        // We do some fancy math here. Basically, any point in time, the amount of KSWAPs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accGoosePerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accKswapPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
         //   1. The pool's `accGoosePerShare` (and `lastRewardBlock`) gets updated.
@@ -35,21 +35,21 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;                 // Address of LP token contract.
-        uint256 allocPoint;             // How many allocation points assigned to this pool. GOOSEs to distribute per block.
-        uint256 lastRewardBlock;        // Last block number that GOOSEs distribution occurs.
-        uint256 accGoosePerShare;       // Accumulated GOOSEs per share, times 1e12. See below.
+        uint256 allocPoint;             // How many allocation points assigned to this pool. KSWAPs to distribute per block.
+        uint256 lastRewardBlock;        // Last block number that KSWAPs distribution occurs.
+        uint256 accKswapPerShare;       // Accumulated KSWAPs per share, times 1e12. See below.
         uint256 depositFeeBP;           // Deposit fee in basis points
         uint256 maxDepositAmount;       // Maximum deposit quota (0 means no limit)
         uint256 currentDepositAmount;   // Current total deposit amount in this pool
     }
 
     // The reward token
-    IMintable public goose;
+    IMintable public kswap;
     // Dev address.
     address public devAddress;
-    // GOOSE tokens created per block.
+    // KSWAP tokens created per block.
     uint256 public goosePerBlock;
-    // Bonus multiplier for early goose makers.
+    // Bonus multiplier for early kswap makers.
     uint256 public constant BONUS_MULTIPLIER = 1;
     // Deposit Fee address
     address public feeAddress;
@@ -60,7 +60,7 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when GOOSE mining starts.
+    // The block number when KSWAP mining starts.
     uint256 public startBlock;
 
     event Harvest(address indexed user, uint256 indexed pid, uint256 amount);
@@ -72,16 +72,16 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
     event UpdateEmissionRate(address indexed user, uint256 goosePerBlock);
 
     constructor(
-        IMintable _goose,
+        IMintable _kswap,
         address _devAddress,
         address _feeAddress,
         uint256 _goosePerBlock,
         uint256 _startBlock
     ) public {
-        goose = _goose;
+        kswap = _kswap;
         devAddress = _devAddress;
         feeAddress = _feeAddress;
-        goosePerBlock = _goosePerBlock;
+        kswapPerBlock = _kswapPerBlock;
         startBlock = _startBlock;
     }
 
@@ -101,14 +101,14 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accGoosePerShare: 0,
+            accKswapPerShare: 0,
             depositFeeBP: _depositFeeBP,
             maxDepositAmount: _maxDepositAmount,
             currentDepositAmount: 0
         }));
     }
 
-    // Update the given pool's GOOSE allocation point and deposit fee. Can only be called by the owner.
+    // Update the given pool's KSWAP allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, uint256 _maxDepositAmount, bool _withUpdate) override external onlyOwner {
         require(_depositFeeBP <= 2000, "add: FEES CANNOT EXCEED 20%");
         if (_withUpdate) {
@@ -125,18 +125,18 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
-    // View function to see pending GOOSEs on frontend.
-    function pendingGoose(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending KSWAPs on frontend.
+    function pendingKswap(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accGoosePerShare = pool.accGoosePerShare;
+        uint256 accKswapPerShare = pool.accKswapPerShare;
         uint256 lpSupply = pool.currentDepositAmount;
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 gooseReward = multiplier.mul(goosePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accGoosePerShare = accGoosePerShare.add(gooseReward.mul(1e12).div(lpSupply));
+            uint256 kswapReward = multiplier.mul(kswapPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accKswapPerShare = accKswapPerShare.add(kswapReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accGoosePerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accKswapPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     function massUpdatePools() override external {
@@ -164,21 +164,21 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
         uint256 gooseReward = multiplier.mul(goosePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        goose.mint(devAddress, gooseReward.div(10));
-        goose.mint(address(this), gooseReward);
-        pool.accGoosePerShare = pool.accGoosePerShare.add(gooseReward.mul(1e12).div(lpSupply));
+        kswap.mint(devAddress, kswapReward.div(10));
+        kswap.mint(address(this), kswapReward);
+        pool.accKswapPerShare = pool.accKswapPerShare.add(kswapReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit tokens to chef for GOOSE allocation.
+    // Deposit tokens to chef for KSWAP allocation.
     function deposit(uint256 _pid, uint256 _amount) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accGoosePerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accKswapPerShare).div(1e12).sub(user.rewardDebt);
             if (pending > 0) {
-                safeGooseTransfer(msg.sender, pending);
+                safeKswapTransfer(msg.sender, pending);
             }
         }
         if (_amount > 0) {
@@ -198,7 +198,7 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
             user.amount = user.amount.add(depositAmount);
             pool.currentDepositAmount = pool.currentDepositAmount.add(depositAmount);
         }
-        user.rewardDebt = user.amount.mul(pool.accGoosePerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accKswapPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -208,16 +208,16 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accGoosePerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accKswapPerShare).div(1e12).sub(user.rewardDebt);
         if (pending > 0) {
-            safeGooseTransfer(msg.sender, pending);
+            safeKswapTransfer(msg.sender, pending);
         }
         if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.currentDepositAmount = pool.currentDepositAmount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accGoosePerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accKswapPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -233,16 +233,16 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe goose transfer function, just in case if rounding error causes pool to not have enough GOOSEs.
-    function safeGooseTransfer(address _to, uint256 _amount) internal {
-        uint256 gooseBal = goose.balanceOf(address(this));
+    // Safe goose transfer function, just in case if rounding error causes pool to not have enough KSWAPs.
+    function safeKswapTransfer(address _to, uint256 _amount) internal {
+        uint256 kswapBal = kswap.balanceOf(address(this));
         bool transferSuccess = false;
-        if (_amount > gooseBal) {
-            transferSuccess = goose.transfer(_to, gooseBal);
+        if (_amount > kswapBal) {
+            transferSuccess = kswap.transfer(_to, gooseBal);
         } else {
-            transferSuccess = goose.transfer(_to, _amount);
+            transferSuccess = kswap.transfer(_to, _amount);
         }
-        require(transferSuccess, "safeGooseTransfer: transfer failed");
+        require(transferSuccess, "safeKswapTransfer: transfer failed");
     }
 
     function setDevAddress(address _devAddress) external {
@@ -258,10 +258,10 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
     }
 
     //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
-    function updateEmissionRate(uint256 _goosePerBlock) external onlyOwner {
+    function updateEmissionRate(uint256 _kswapPerBlock) external onlyOwner {
         _massUpdatePools();
-        goosePerBlock = _goosePerBlock;
-        emit UpdateEmissionRate(msg.sender, _goosePerBlock);
+        kswapPerBlock = _kswapPerBlock;
+        emit UpdateEmissionRate(msg.sender, _kswapPerBlock);
     }
 
     //New function to trigger harvest for a specific user and pool
@@ -274,10 +274,10 @@ contract IncubatorChef is Ownable, ReentrancyGuard, IIncubatorChef  {
         UserInfo storage user = userInfo[_pid][_user];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accGoosePerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accKswapPerShare).div(1e12).sub(user.rewardDebt);
             if (pending > 0) {
-                safeGooseTransfer(_user, pending);
-                user.rewardDebt = user.amount.mul(pool.accGoosePerShare).div(1e12);
+                safeKswapTransfer(_user, pending);
+                user.rewardDebt = user.amount.mul(pool.accKswapPerShare).div(1e12);
                 emit Harvest(_user, _pid, pending);
             }
         }
